ed795b5c961e4fc2247e4b5d8c989321279d66a3
Souphan <souphan.ctr@gmail.com>
2015-11-18 16:37:33 -0600

    project-week3

--- app.js
+++ app.js
@@ -1,4 +1,61 @@
 $(document).ready(function() {
+  $("button").click(function(){
+    $(".lead").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px',
+        
+        
+    });
+  });
     
+    var introPage = document.getElementsByClassName('lead') {
     
+}
+ $(".web-border").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px',
+     
+ });
 });
+
+
+//Roll the img of master-ball to right when page loads
+    //I might need a wrapper for HTML.  There was a wrapper pre my change HTML
+     
+     /*
+     
+             
+var introPage = document.getElementsByClassName('lead') {
+    
+}
+ $(".web-border").animate({
+        left: '250px',
+        opacity: '0.5',
+        height: '150px',
+        width: '150px',
+     
+     website about health information and transit issues.
+     Need a website.
+     education resources that aren't charged.
+     
+     Local resources that we have and educating ways to get outdoors in the community.
+     
+     We have the kansas health foundation that have webesites with a little bit of information.
+     
+     Create something website wise to have access to that information.
+     
+     Place to upload information.  Publish other peoples information.
+     
+     end goal is for content management systems.  Word Press wouldn't be too hard.
+     
+     Search function to find information.
+     
+     Things relevant to wichita for health.
+     
+     Health professionals about providing information for free.
+     
+     */
