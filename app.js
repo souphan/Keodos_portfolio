4bcfb9baa7cb97289fddd67ac7db3f174732f1c9
Souphan <souphan.ctr@gmail.com>
2015-12-04 22:25:55 -0600

    week6 completed portfolio and most of about me and contact info

--- app.js
+++ app.js
@@ -1,68 +1,10 @@
- $(document).ready(function() {
+$("#slideshow > div:gt(0)").hide();
 
-            $("#in").click(function(){
-               $(".lead").fadeIn( 'slow', function(){ 
-                  $(".log").text('Fade In Transition Complete');
-               });
-
-            $("#out").click(function(){
-               $(".lead").fadeOut( 'slow', function(){ 
-                  $(".log").text('Fade Out Transition Complete');
-               });
-				
-         });
-            });
- });
-/*   var introPage = document.getElementsByClassName('lead') {
-    
-}
-$(document).ready(function() {
-  $("button").click(function(){
-     $(".lead").animate({
-        left: '250px',
-        opacity: '0.5',
-        height: '500px',
-        width: '500px',
-     
- });
-});
-});
-
-*/
-
-//Roll the img of master-ball to right when page loads
-    //I might need a wrapper for HTML.  There was a wrapper pre my change HTML
-     
-     /*
-     
-             
-var introPage = document.getElementsByClassName('lead') {
-    
-}
- $(".web-border").animate({
-        left: '250px',
-        opacity: '0.5',
-        height: '150px',
-        width: '150px',
-     
-     website about health information and transit issues.
-     Need a website.
-     education resources that aren't charged.
-     
-     Local resources that we have and educating ways to get outdoors in the community.
-     
-     We have the kansas health foundation that have webesites with a little bit of information.
-     
-     Create something website wise to have access to that information.
-     
-     Place to upload information.  Publish other peoples information.
-     
-     end goal is for content management systems.  Word Press wouldn't be too hard.
-     
-     Search function to find information.
-     
-     Things relevant to wichita for health.
-     
-     Health professionals about providing information for free.
-     
-     */
+setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+},  3000);
---
+++ contact.md
@@ -0,0 +1,7 @@
+<form action="https://getsimpleform.com/messages?form_api_token=fde4bcbf8129ae28a5403fca8650fd97" method="post">
+  <!-- the redirect_to is optional, the form will redirect to the referrer on submission -->
+  <input type='hidden' name='redirect_to' value='http://souphan.github.io/thank-you' />
+  <!-- all your input fields here.... -->
+  <input type='text' name='test' />
+  <input type='submit' value='Test form' onclick="return confirm('Are you sure?')"  />
+</form>
